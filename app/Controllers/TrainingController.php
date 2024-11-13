4e0ea3d493bdd143a87599301ee70e11b8d7e464
AldoAgustio333 <aldoagustio333@gmail.com>
2024-11-13 11:41:12 +0700

    first commit

---
+++ app/Controllers/TrainingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class TrainingController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
